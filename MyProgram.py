8564f836e51dc5e08db05bddd1ecf6a19ea58448
294250lama-design <294250.lama@gmail.com>
2025-10-31 22:03:02 -0500

    Create MyProgram.py

---
+++ MyProgram.py
@@ -0,0 +1,23 @@
+public class MyProgram
+{
+    public static void main(String[] args)
+    {
+String s = "";
+int n = 0;
+for(int i = 0; i<2;i++)
+{
+   while(n<3)
+   {
+      s = "*" + s;
+      n++;
+   }
+   while(n>0)
+   {
+      s += "-";
+      n--;
+   }
+   s+="\n";
+}
+System.out.print(s);
+        }
+    }
